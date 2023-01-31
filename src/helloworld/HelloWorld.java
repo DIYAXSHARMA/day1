f5d5afa53b130a7ec7b1981c8d115e7145e8728c
sdiya <sdiya@10.48.43.229>
2023-01-31 14:14:04 -0500

    myfirst comit

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * Click  to change this license
+ * Click to edit this template
+ */
+package helloworld;
+
+/**
+ *
+ * @author diya sharma
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ji");
+    }
+    
+}
